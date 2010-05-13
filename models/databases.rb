530c43660760a31a7482f54d211bfbc63eb44efa
Sirupsen <sirup@sirupsen.dk>
2010-05-13 10:56:55 +0200

    Cleaned up model

--- models/databases.rb
+++ models/databases.rb
@@ -4,21 +4,22 @@ class Database
 
   def initialize(name)
     @name = name
-    @collections
   end
 
   def self.find(query)
     db = Mongo::Connection.new
+
     @@databases = []
-    db.database_names.each { |name| @@databases << Database.new(name) }
+    db.database_names.each {|name| @@databases << Database.new(name)}
     find_collections_for_databases
     @@databases
   end
 
-  def self.find_collections_for_databases
-    @@databases.each do |db|
-      tmp_db = Mongo::Connection.new.db(db.name)
-      db.collections = tmp_db.collection_names
+  private
+    def self.find_collections_for_databases
+      @@databases.each do |db|
+        tmp_db = Mongo::Connection.new.db(db.name)
+        db.collections = tmp_db.collection_names
+      end
     end
-  end
 end
