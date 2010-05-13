017181f4b2a2f4d99e1a669aad893b3eaaeefc43
Sirupsen <sirup@sirupsen.dk>
2010-05-13 11:09:00 +0200

    Model now stores all DB info, view to reflect.

--- models/databases.rb
+++ models/databases.rb
@@ -1,5 +1,7 @@
+require 'mongo'
+
 class Database
-  attr_accessor :collections
+  attr_accessor :collections, :db
   attr_reader :name
 
   def initialize(name)
@@ -8,18 +10,21 @@ class Database
 
   def self.find(query)
     db = Mongo::Connection.new
+    
 
-    @@databases = []
-    db.database_names.each {|name| @@databases << Database.new(name)}
-    find_collections_for_databases
-    @@databases
+    if query = :all
+      @@databases = []
+      db.database_names.each {|name| @@databases << Database.new(name)}
+      find_collections_for_databases
+      @@databases
+    end
   end
 
   private
     def self.find_collections_for_databases
       @@databases.each do |db|
         tmp_db = Mongo::Connection.new.db(db.name)
-        db.collections = tmp_db.collection_names
+        db.db = tmp_db
       end
     end
 end
