a0d1743341e8e9dcde76e5b3dd3c6738f9f70a2b
charliesome <charlie@charliesomerville.com>
2010-06-19 14:38:38 +1000

    documents can now be edited in place

--- mongui.rb
+++ mongui.rb
@@ -35,6 +35,30 @@ get "/add/document/:database/:collection" do
   haml :add_document, {:layout => true}
 end
 
+get "/edit/document/:database/:collection/:id" do
+  @db = Database.find(params[:database])
+  @collection = @db.db[params[:collection]]
+  @doc = @collection.find_one({:_id => BSON::ObjectID.from_string(params[:id])})
+
+  if @doc
+    haml :edit_document, {:layout => true}
+  else
+    redirect "/database/#{params[:database]}/#{params[:collection]}"
+  end
+end
+
+post "/edit/document/:database/:collection/:id" do
+  @db = Database.find(params[:database])
+  @collection = @db.db[params[:collection]]
+  @doc = @collection.find_one({:_id => BSON::ObjectID.from_string(params[:id])})
+
+  if @doc
+     @collection.update({:_id => @doc["_id"]}, params[:document])
+  end
+
+  redirect "/database/#{params[:database]}/#{params[:collection]}"
+end
+
 post "/add/document/:database/:collection" do
   @db = Database.find(params[:database])
   @collection = @db.db[params[:collection]]
